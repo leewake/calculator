af632d238b2b9d2b03e22e10ed17a8ca28e91f7d
lijingwei <jli8@xiaohongshu.com>
2020-03-20 15:38:27 +0800

    修改启动入口

--- src/main/java/com/leewake/Main.java
+++ src/main/java/com/leewake/Main.java
@@ -6,41 +6,27 @@ import com.leewake.rpn.RPNException;
 import java.util.Scanner;
 
 /**
- * @description: 计算器
- * @author: leewake
- * @create: 2020-03-19 11:04
- **/
-
+ * <B>Description:</B> 项目入口 <br>
+ * <B>Create on:</B> 2020/3/20 下午3:35 <br>
+ *
+ * @author leewake
+ */
 public class Main {
 
-    // 批测试用的表达式
-    static String[] testExpression = new String[] { "5 2", "2 sqrt", "clear 9 sqrt", "5 2 -", "-", "clear", "5 4 3 2",
-            "undo undo *", "5 *", "undo", "7 12 2 /", "*", "4 /", "1 2 3 4 5 *", "clear 3 4 -", "1 2 3 4 5", "* * * *",
-            "1 2 3 * 5 + * * 6 5" };
-
     public static void main(String args[]) {
-        RPNCalculator cl = new RPNCalculator();
-        boolean flag = true;// 批测开关
+        RPNCalculator calculator = new RPNCalculator();
+        Scanner scanner = new Scanner(System.in);
         try {
-            while (true) {
-                // 使用批量表达式测试
-                if (flag) {
-                    for (String d : testExpression) {
-                        System.out.println("Input expression: " + d);
-                        cl.calByRPN(d);
-                    }
-                    flag = false;
-                }
-                // 手动输入表达式测试
-                System.out.println("Please enter an expression: ");
-                Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNext()) {
+                // 手动输入表达式
+//                System.out.println("Please enter an expression: ");
                 String rpn = scanner.nextLine();
-                System.out.println("Input expression: " + rpn);
-                cl.calByRPN(rpn);
+//                System.out.println("Input expression: " + rpn);
+                calculator.calByRPN(rpn);
             }
-
         } catch (RPNException e) {
             e.printStackTrace();
         }
     }
+
 }
